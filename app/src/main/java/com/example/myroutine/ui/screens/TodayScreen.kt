410331c3f6873cd4c4bebe652d93388c428252b0
Nongju-3000 <kangjongwoo333@yonsei.ac.kr>
2025-05-23 22:47:37 +0900

    feat: TodayScreen 컴포저블 추가
    `TodayScreen.kt` 파일에 "Today's Tasks Here" 텍스트를 표시하는 간단한 `TodayScreen` 컴포저블 함수를 추가합니다.

---
+++ app/src/main/java/com/example/myroutine/ui/screens/TodayScreen.kt
@@ -0,0 +1,9 @@
+package com.example.myroutine.ui.theme
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TodayScreen() {
+    Text("Today’s Tasks Here")
+}
