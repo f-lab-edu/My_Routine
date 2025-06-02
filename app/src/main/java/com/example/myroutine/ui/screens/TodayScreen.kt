c939c6fbe7bc9725bc24d5210248527ab7906d15
Nongju-3000 <kangjongwoo333@yonsei.ac.kr>
2025-06-03 02:40:05 +0900

    - Routes 및 RoutineItem import 경로 변경

--- app/src/main/java/com/example/myroutine/ui/screens/TodayScreen.kt
+++ app/src/main/java/com/example/myroutine/ui/screens/TodayScreen.kt
@@ -20,7 +20,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
-import com.example.myroutine.data.RoutineItem
+import com.example.myroutine.data.local.entity.RoutineItem
 
 @Composable
 fun TodayScreen() {
