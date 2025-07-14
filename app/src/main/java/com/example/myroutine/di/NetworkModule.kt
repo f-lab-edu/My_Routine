cda3c57bb51249388ecf73e59dbefeced0cf81a6
Nongju-3000 <kangjongwoo333@yonsei.ac.kr>
2025-07-14 21:06:33 +0900

    refactor: NetworkModule.kt BASE_URL 및 OkHttpClient 설정 변경 (#12)
    - BASE_URL을 HTTPS로 변경
    - OkHttpClient에 HttpLoggingInterceptor 추가

--- app/src/main/java/com/example/myroutine/di/NetworkModule.kt
+++ app/src/main/java/com/example/myroutine/di/NetworkModule.kt
@@ -16,7 +16,7 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
 
-    private const val BASE_URL = "http://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/"
+    private const val BASE_URL = "https://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/"
 
     @Provides
     @Singleton
