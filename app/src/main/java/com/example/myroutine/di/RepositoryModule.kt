e7babe19fbaaf351c763893bc80f126c6ada4ea4
Nongju-3000 <kangjongwoo333@yonsei.ac.kr>
2025-07-14 21:06:55 +0900

    feat: RepositoryModule.kt 추가 및 RoutineRepository 바인딩 (#12)
    - RoutineRepositoryImpl을 Hilt에 제공하는 RepositoryModule 추가

--- app/src/main/java/com/example/myroutine/di/RepositoryModule.kt
+++ app/src/main/java/com/example/myroutine/di/RepositoryModule.kt
@@ -2,6 +2,7 @@ package com.example.myroutine.di
 
 import com.example.myroutine.data.local.dao.RoutineCheckDao
 import com.example.myroutine.data.local.dao.RoutineDao
+import com.example.myroutine.data.repository.HolidayRepository
 import com.example.myroutine.data.repository.RoutineRepository
 import com.example.myroutine.data.repository.RoutineRepositoryImpl
 import dagger.Module
@@ -15,8 +16,9 @@ object RepositoryModule {
     @Provides
     fun provideRoutineRepository(
         routineDao: RoutineDao,
-        checkDao: RoutineCheckDao
+        checkDao: RoutineCheckDao,
+        holidayRepository: HolidayRepository
     ): RoutineRepository {
-        return RoutineRepositoryImpl(routineDao, checkDao)
+        return RoutineRepositoryImpl(routineDao, checkDao, holidayRepository)
     }
 }
