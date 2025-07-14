15cafbe862d04090269fa232da47af5168bd91db
Nongju-3000 <kangjongwoo333@yonsei.ac.kr>
2025-07-14 21:06:10 +0900

    refactor: HolidayApiService.kt API 경로 및 파라미터 변경 (#12)
    - 공공데이터 API 변경에 맞춰 @GET 경로 및 파라미터 수정

--- app/src/main/java/com/example/myroutine/data/source/remote/HolidayApiService.kt
+++ app/src/main/java/com/example/myroutine/data/source/remote/HolidayApiService.kt
@@ -5,12 +5,10 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface HolidayApiService {
-    @GET("getAnniversaryInfo")
+    @GET("getRestDeInfo")
     suspend fun getHolidayInfo(
         @Query("serviceKey") serviceKey: String,
-        @Query("pageNo") pageNo: Int,
-        @Query("numOfRows") numOfRows: Int,
         @Query("solYear") solYear: Int,
-        @Query("solMonth") solMonth: Int
+        @Query("solMonth") solMonth: String
     ): HolidayDto
 }
